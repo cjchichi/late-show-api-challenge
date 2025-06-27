8a2a86dca8cb09a5130a78c1b4ea2e743f100fa9
cjchichi <cynthia1973chichi@gmail.com>
2025-06-27 17:38:20 +0300

    add server

--- server/controllers/episode_controller.py
+++ server/controllers/episode_controller.py
@@ -1,4 +1,4 @@
-    from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required
 from models import db
 from models.episode import Episode
